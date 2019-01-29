8d526d1b9a3b76449ba57b98e9ecb57e8316230b
Oscar Rivera <o.riveraruiz@gmail.com>
2019-01-29 08:39:47 +0100

    Inicio

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Tema Trucker Box",
+    "summary": "Tema personalizado para Carlos Perez Frias",
+    "version": "1.0.0",
+    "category": "Theme/Creative",
+    "website": "https://www.mycloudskin.com",
+    "author": "Oscar Rivera Ruiz",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "images": [
+        "static/description/cloud.png",
+    ],
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/layout.xml",
+        "views/assets.xml",
+        "views/snippets.xml",
+    ],
+}
